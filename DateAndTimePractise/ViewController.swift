b913c346e8402c1007da3c0dd6575d336a8b13e8
suryaspidy <ss0972203@gmail.com>
2021-06-07 10:50:50 +0530

    Initial Commit

---
+++ DateAndTimePractise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DateAndTimePractise
+//
+//  Created by surya-zstk231 on 07/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
